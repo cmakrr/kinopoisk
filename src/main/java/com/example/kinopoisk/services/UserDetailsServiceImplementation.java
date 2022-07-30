4decf0e37c478e1f475db65050ba9e45e16dffba
hahahalol9 <cmak7543@mail.ru>
2022-07-30 18:53:50 +0300

    chore: add service annotation to user details service

--- src/main/java/com/example/kinopoisk/services/UserDetailsServiceImplementation.java
+++ src/main/java/com/example/kinopoisk/services/UserDetailsServiceImplementation.java
@@ -1,15 +1,16 @@
 package com.example.kinopoisk.services;
 
-import com.example.kinopoisk.models.User;
+import com.example.kinopoisk.models.entities.User;
 import com.example.kinopoisk.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public record UserDetailsServiceImplementation(UserRepository userRepository) implements UserDetailsService {
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = userRepository.findByUsername(username);
