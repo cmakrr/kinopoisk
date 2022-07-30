82c000035b9d9f8ff0e3230299e88a8b4f1d93ab
hahahalol9 <cmak7543@mail.ru>
2022-07-30 20:34:12 +0300

    test: add user service test

---
+++ src/test/java/com/example/kinopoisk/services/UserServiceTest.java
@@ -0,0 +1,93 @@
+package com.example.kinopoisk.services;
+
+import com.example.kinopoisk.models.entities.Role;
+import com.example.kinopoisk.models.entities.User;
+import com.example.kinopoisk.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@ContextConfiguration(classes = UserService.class)
+@TestPropertySource("classpath:/properties/user.properties")
+class UserServiceTest {
+    @InjectMocks
+    private UserService userService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Value("${defaultAvatarName}")
+    private String defaultAvatarName;
+    @Value("${defaultUserRoleOrd}")
+    private int defaultRoleOrd;
+
+    @Test
+    void saveNewUser() {
+        User user = new User();
+        String username = "username";
+        String password = "password";
+        String encodedPassword = "encodedPassword";
+
+        user.setPassword(password);
+        user.setUsername(username);
+
+        when(passwordEncoder.encode(password)).thenReturn(encodedPassword);
+
+        userService.saveNewUser(user);
+        ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(argumentCaptor.capture());
+
+        Set<Role> expectedRoles = getUserExpectedRolesSet();
+        User actualUser = argumentCaptor.getValue();
+
+        Assertions.assertEquals(username,actualUser.getUsername());
+        Assertions.assertEquals(encodedPassword,actualUser.getPassword());
+        Assertions.assertEquals(expectedRoles,actualUser.getRoles());
+        Assertions.assertEquals(defaultAvatarName,actualUser.getAvatarName());
+    }
+
+    private Set<Role> getUserExpectedRolesSet(){
+        Role userRole = Role.values()[defaultRoleOrd];
+        return Collections.singleton(userRole);
+    }
+
+    @Test
+    void isUsernameUnique_UniqueUsername() {
+       String username = "username";
+
+       when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+       boolean expected = true;
+       boolean actual = userService.isUsernameUnique(username);
+
+       Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void isUsernameUnique_NotUniqueUsername(){
+        String username = "username";
+
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(new User()));
+
+        boolean expected = false;
+        boolean actual = userService.isUsernameUnique(username);
+
+        Assertions.assertEquals(expected,actual);
+    }
+}
