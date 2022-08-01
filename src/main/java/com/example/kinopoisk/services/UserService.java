9d7d8638a13e85333e39fbba46ddfb87f8e88017
hahahalol9 <cmak7543@mail.ru>
2022-08-01 15:40:21 +0300

    chore: update user service (add methods to work with user dto)

--- src/main/java/com/example/kinopoisk/services/UserService.java
+++ src/main/java/com/example/kinopoisk/services/UserService.java
@@ -1,16 +1,20 @@
 package com.example.kinopoisk.services;
 
+import com.example.kinopoisk.logic.dtoConverters.UserDTOConverter;
 import com.example.kinopoisk.models.dtos.UserDTO;
 import com.example.kinopoisk.models.entities.Role;
 import com.example.kinopoisk.models.entities.User;
 import com.example.kinopoisk.repositories.UserRepository;
+import com.example.kinopoisk.services.imagesSaving.ImageOperations;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -19,6 +23,8 @@ import java.util.Set;
 public class UserService{
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserDTOConverter userDTOConverter;
+    private final ImageOperations imageOperations;
     @Value("${defaultAvatarName}")
     private String defaultAvatarName;
     @Value("${defaultUserRoleOrd}")
@@ -46,13 +52,39 @@ public class UserService{
         user.setPassword(encodedPassword);
     }
 
+    public void setAvatar(UserDTO user, MultipartFile avatar){
+        if(!avatar.isEmpty()){
+            Optional<String> avatarName = imageOperations.saveImage(avatar);
+            avatarName.ifPresent(user::setAvatarName);
+        }
+    }
+
+    public boolean isCurrentUser(Long id){
+        return receiveCurrentUserId().equals(id);
+    }
+
+    public Long receiveCurrentUserId(){
+        User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return user.getId();
+    }
+
+    public Optional<UserDTO> findDTOById(Long id){
+        Optional<User> user = userRepository.findById(id);
+        return user.map(userDTOConverter::convertToDTO);
+    }
+
     public void updateUserByDTO(UserDTO userDTO){
-        //User user = userDTOConverter.convertFromDTO(userDTO);
-        //userRepository.save(user);
+        User user = userDTOConverter.convertFromDTO(userDTO);
+        userRepository.save(user);
     }
 
     public void deleteUser(Long id){
         userRepository.deleteById(id);
+    }
+
+    public boolean isNewUsernameAppropriate(UserDTO userDTO){
+        Optional<User> userWithNewName = userRepository.findByUsername(userDTO.getUsername());
+        return userWithNewName.isEmpty() || userWithNewName.get().getId().equals(userDTO.getId());
     }
 
     public boolean isUsernameUnique(String username){
