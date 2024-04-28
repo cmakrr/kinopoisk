541366e4a756e03d3154c70d34a92d5689cb8b6c
cmakrr <cmakreal7@mail.ru>
2024-04-28 13:42:15 +0300

    Finish styles refactoring

--- src/main/java/com/example/kinopoisk/model/entities/show/ShowType.java
+++ src/main/java/com/example/kinopoisk/model/entities/show/ShowType.java
@@ -1,7 +1,7 @@
 package com.example.kinopoisk.model.entities.show;
 
 public enum ShowType {
-    FILM("film"),SERIES("tv series"),ANIME("anime series");
+    FILM("film"),SERIES("tv series"),ANIME("anime series"),GAME("game");
 
     private final String name;
 
